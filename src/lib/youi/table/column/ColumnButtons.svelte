4ffd8199862b52c128b527e1c73ce120c47b3dca
zhyi12 <zhouyi>
2024-04-26 10:15:18 +0800

    新增表格列按钮组件

---
+++ src/lib/youi/table/column/ColumnButtons.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+    import Button from "../../button/Button.svelte";
+    import type {ButtonInfo} from "../../button/Button.svelte";
+    import {isFunction} from "@tanstack/table-core";
+
+    export let buttons:ButtonInfo[] = [];
+
+    export let record:Record<string, any> = {};
+
+    /**
+     *
+     * @param button
+     */
+    const doAction = (button) => {
+        if(isFunction(button.action)){
+            button.action(record);
+        }
+    }
+
+</script>
+
+<div>
+    {#each buttons as button}
+        <Button title={`${button.title||button.text}${record.caption}`} class="btn-sm btn-ghost" on:click={()=>doAction(button)}>
+            {button.text}
+        </Button>
+    {/each}
+</div>
