ca8231197e1334b03c30dd43df4c6b0f6aa4d8c3
zhyi12 <zhouyi>
2024-04-27 20:10:54 +0800

    feat: add 集合转树结构方法

--- src/lib/app-services/service.util.ts
+++ src/lib/app-services/service.util.ts
@@ -1,4 +1,4 @@
-import type {Pager} from "../app-dao/dao";
+
 import {
     appDataBase,
     buildConditions,
@@ -7,13 +7,18 @@ import {
     wrapSqlCount, wrapSqlLimit
 } from "../app-dao/dao.util";
 
+import type {TreeItem} from "../youi/index.d";
+import type {TreeObject} from "../app-entity/entity";
+import type {Pager} from "../app-dao/dao";
+import {findTreeNode, removeTreeNode} from "../youi/util/tree.util";
+
 /**
  * 通用分页查询
  * @param pager
  * @param selectSql
  * @param entity
  */
-export async  function findByPager<T>(pager:Pager,selectSql,entity?:T) {
+export async function findByPager<T>(pager:Pager,selectSql,entity?:T) {
     const conditions = buildConditions(entity);
     const querySql = conditions.length?`${selectSql} where ${buildConditionsSql(conditions)}`:selectSql;
     let bindValues:any[] = conditions.map(condition=>condition.value);
@@ -32,4 +37,79 @@ export async  function findByPager<T>(pager:Pager,selectSql,entity?:T) {
         totalCount,
         records
     }
+}
+
+/**
+ *
+ */
+export function levelListToTree<T extends TreeObject>(list:T[],nodeProcessor?:(TreeItem,T)=>void):TreeItem[]{
+    if(list.length === 0 )return [];
+
+    const treeNodes:TreeItem[] = [];
+    const levelMaps:Record<String,TreeItem> = {};
+    const topLevel = list[0].level;
+
+    for(let i=0;i<list.length;i++){
+        const obj = list[i];
+        const treeItem:TreeItem = {
+            id:obj.id.toString(),
+            text:obj.text,
+            datas:{
+                dataId:obj.id,
+                num:obj.num
+            }
+        }
+
+        if(nodeProcessor){
+            nodeProcessor(treeItem,obj);
+        }
+
+        const levelKey = buildLevelKey(obj.level||1);
+
+        levelMaps[levelKey] = treeItem;
+
+        if(topLevel === obj.level){
+            treeNodes.push(treeItem);
+        }else{
+            const parentLevelKey = buildLevelKey((obj.level||1)-1);
+            const parentNode:TreeItem = levelMaps[parentLevelKey];
+            if(parentNode){
+                parentNode.children = parentNode.children||[];
+                parentNode.children.push(treeItem);
+            }
+        }
+    }
+
+    return treeNodes
+}
+
+
+
+/**
+ *
+ */
+export function moveTreeNode(treeNodes:TreeItem[],source:TreeItem,target:TreeItem,position:string,newNum:number){
+    //
+    const sourceTreeNode = findTreeNode(treeNodes,source.id);
+    // sourceTreeNode.datas.num = newNum;
+    // removeTreeNode(treeNodes,source.id,null);
+    // const pathNodes = findPathNodes(treeNodes,target.id,[]);
+    //
+    // if('before' === position || 'after' === position){
+    //     if(pathNodes.length>1){
+    //         const parent = pathNodes[pathNodes.length-2];
+    //         parent.children = parent.children ||[];
+    //         parent.children.push(sourceTreeNode);
+    //     }else{
+    //         treeNodes.push(sourceTreeNode);
+    //     }
+    // }else{
+    //     const parent = pathNodes.pop();
+    //     parent.children = parent.children ||[];
+    //     parent.children.push(sourceTreeNode);
+    // }
+}
+
+function buildLevelKey(level:number) {
+    return `L${level}`;
 }
