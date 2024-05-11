0f943a583c13d5b1b482a1f9a236f495f0133487
zhyi12 <zhouyi>
2024-05-11 15:03:11 +0800

    feat: 实现页面导航

--- src/lib/app-page/Header.svelte
+++ src/lib/app-page/Header.svelte
@@ -1,33 +1,15 @@
 <script>
-    import {Icon,homeIcon,folderOIcon,hddOIcon} from "../youi";
+
     import PageNav from "./PageNav.svelte";
-
+    import {configStore} from "../app-stores/base/configStore";
     export let navPaths = [];
-
 
 </script>
 
 <div class="shadow items-center h-12 flex shrink-0 pl-2">
-    <div class="text-sm breadcrumbs">
-        <PageNav {navPaths}/>
-<!--        <ul>-->
-<!--            <li>-->
-<!--                <a>-->
-<!--                    <Icon data={homeIcon} class="w-4 h-4"/>-->
-<!--                    首页-->
-<!--                </a>-->
-<!--            </li>-->
-<!--            <li>-->
-<!--                <a>-->
-<!--                    <Icon data={hddOIcon} class="w-4 h-4"/>-->
-<!--                    云盘-->
-<!--                </a>-->
-<!--            </li>-->
-<!--            <li>-->
-<!--              <span class="inline-flex gap-1 items-center">-->
-<!--                <Icon data={folderOIcon} class="w-4 h-4"/>文件夹-->
-<!--              </span>-->
-<!--            </li>-->
-<!--        </ul>-->
+    <PageNav {navPaths}/>
+    <div class="flex-1"></div>
+    <div>
+        {$configStore.areaId||''}
     </div>
 </div>
--- src/lib/app-page/PageNav.svelte
+++ src/lib/app-page/PageNav.svelte
@@ -1,23 +1,56 @@
 <script>
 
-    import {Icon} from "../youi";
+    import {Button,Icon} from "../youi";
     import {APP_ICONS} from "../app-icons/icons";
+    import {backwardIcon,forwardIcon} from "../app-icons";
+    import {getContext} from "svelte";
+
+    const {canBack,canForward} = getContext("AppContext");
 
     export let navPaths = [];
 
+    let backCount = 0;
+
+    const goBack = () => {
+        window.history.back();
+        backCount++;
+        $canForward = true;
+        if(backCount>=window.history.length-1){
+            $canBack = false;
+        }
+    }
+
+    const goForward = ()=>{
+        window.history.forward();
+        backCount--;
+        if(backCount <= 0){
+            $canForward = false;
+        }
+    }
+
 </script>
 
-<ul>
+<Button class="btn-ghost btn-xs p-1" disabled={!$canBack} title="后退" on:click={()=>goBack()}>
+    <Icon data={backwardIcon} class="w-4 h-4"/>
+</Button>
+<Button class="btn-ghost btn-xs p-1 " disabled={!$canForward} on:click={()=>goForward()} title="前进">
+    <Icon data={forwardIcon} class="w-4 h-4"/>
+</Button>
+<div class="text-sm breadcrumbs">
 
-    {#each navPaths as path,index}
-        <li>
-            <a href={path.href}>
-                {#if APP_ICONS[path.icon]}
-                    <Icon data={APP_ICONS[path.icon]} class="w-4 h-4"/>
-                {/if}
-                {path.text}
-            </a>
-        </li>
-    {/each}
+    <ul>
+        {#each navPaths as path,index}
+            {#if path.text}
+                <li>
+                    <a href={path.href}>
+                        {#if APP_ICONS[path.icon]}
+                            <Icon data={APP_ICONS[path.icon]} class="w-4 h-4"/>
+                        {/if}
+                        {path.text}
+                    </a>
+                </li>
+            {/if}
+        {/each}
 
-</ul>
+    </ul>
+</div>
